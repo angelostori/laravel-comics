077e28f5883beb9070b74e761d36a9dd720b2120
angelostori <angelostori13@gmail.com>
2026-02-18 16:43:28 +0100

    update home view layout and styling, update jumbotron and card components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,27 @@
 @extends("layouts.defaultLayout")
 
 @section("content")
-<h1>hello world</h1>
+<main>
+    <section class="jumbotron text-center py-5">
+        <button type="button" class="btn btn-primary">CURRENT SERIES</button>
+    </section>
 
-@foreach ($comics as $comic)
-<x-card>
-    <x-slot:thumb>{{ $comic['thumb'] }}</x-slot:thumb>
-    <x-slot:description>{{ $comic['description'] }}</x-slot:description>
-    {{ $comic['series'] }}
-</x-card>
-@endforeach
+    <section>
+        <div class="container">
+            <div class="row g-4">
+                @foreach (config('comics') as $comic)
+                <x-card>
+                    <x-slot:thumb>{{ asset($comic['thumb']) }}</x-slot:thumb>
+                    <x-slot:description>{{ $comic['description'] }}</x-slot:description>
+                    {{ $comic['series'] }}
+                </x-card>
+                @endforeach
+            </div>
+
+            <div class="text-center my-4">
+                <button type="button" class="btn btn-primary">LOAD MORE</button>
+            </div>
+        </div>
+    </section>
+</main>
 @endsection
