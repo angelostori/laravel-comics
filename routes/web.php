b7399f94f09527c7f55692d3f0a397113ed91bf1
angelostori <angelostori13@gmail.com>
2026-02-18 16:07:02 +0100

    update home view to dynamically display comic details from configuration

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $footerlinks = config('footerlinks');
-    return view('home', compact('footerlinks'));
+    $comics = config('comics');
+    return view('home', compact('footerlinks', 'comics'));
 })->name("home");
 
 Route::get('/details', function () {
