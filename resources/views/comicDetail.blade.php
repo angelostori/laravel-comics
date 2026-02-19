a0cb45f4ab1f7319797910e3d8e3351b81309ad8
angelostori <angelostori13@gmail.com>
2026-02-19 15:20:17 +0100

    update detail container styling and structure in comic detail view

--- resources/views/comicDetail.blade.php
+++ resources/views/comicDetail.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.defaultLayout')
 
 @section('content')
-<div class="container py-5">
+<div class="vw-100 py-5 detail-container">
 
-    <h1>{{ $comic['series'] }}</h1>
+    <div class="container">
+        <h1>{{ $comic['series'] }}</h1>
 
-    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
 
-    <p>{{ $comic['description'] }}</p>
-    <p><strong>Prezzo:</strong> {{ $comic['price'] }}</p>
-    <p><strong>Data:</strong> {{ $comic['sale_date'] }}</p>
-    <p><strong>Tipo:</strong> {{ $comic['type'] }}</p>
+        <p>{{ $comic['description'] }}</p>
+        <p><strong>Prezzo:</strong> {{ $comic['price'] }}</p>
+        <p><strong>Data:</strong> {{ $comic['sale_date'] }}</p>
+        <p><strong>Tipo:</strong> {{ $comic['type'] }}</p>
+    </div>
 
 </div>
 @endsection
